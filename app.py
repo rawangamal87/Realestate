d344cf7a4ae8f02f425ed12555f8765c6724500a
Rawan gamal <137230409+rawangamal87@users.noreply.github.com>
2025-07-25 17:33:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pickle
 import numpy as np
-from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import Tokenizer
 
